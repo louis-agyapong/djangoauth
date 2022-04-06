2b2996026954cb7226c6e03a8f3a203cb81cfc1b
Louis Agyapong <agyapong.louis@gmail.com>
2022-04-06 20:07:08 +0000

    custom user model

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    search_fields = ("email", "username", "first_name", "last_name", "middle_name")
+    list_display = ("email", "username", "first_name", "last_name", "middle_name", "is_active", "is_staff")
+    ordering = ("-date_joined",)
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "email",
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "middle_name",
+                    "date_of_birth",
+                )
+            },
+        ),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login",)}),
+        ("Groups", {"fields": ("groups",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "middle_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
--- apps/account/models.py
+++ apps/account/models.py
@@ -1,2 +1,38 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name=_("email address"), unique=True)
+    username = models.CharField(_("Username"), max_length=256, blank=True)
+    first_name = models.CharField(_("first name"), max_length=256)
+    last_name = models.CharField(_("last name"), max_length=256)
+    middle_name = models.CharField(_("middle name"), max_length=256, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(_("Date joined"), default=timezone.now, editable=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    @property
+    def full_name(self):
+        if self.middle_name:
+            return ("%s %s %s" % (self.first_name, self.middle_name, self.last_name)).strip()
+        return self.email
+
+    def short_name(self):
+        return f"{self.last_name}_{self.first_name}"
+
+    def __str__(self):
+        return self.full_name
+
+    def save(self, *args, **kwargs):
+        self.email = self.email.lower()
+        return super().save(*args, **kwargs)
