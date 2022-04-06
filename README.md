91581785a69831b99509abbf5eb9652c61c41cdf
Louis Agyapong <agyapong.louis@gmail.com>
2022-04-06 17:34:20 +0000

    account app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### Django authentication and authorization playground
+
+1. Custom User Model
--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Local apps
+    "apps.account",
     # External apps
     "django_extensions",
 ]
