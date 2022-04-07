a05ac3c2a5bb9d835a77849f21087bec8f0f5c60
Louis Agyapong <agyapong.louis@gmail.com>
2022-04-07 17:08:55 +0000

    django guardian

--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "apps.inventory",
     # External apps
     "django_extensions",
+    "guardian",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,9 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "account.User"
+
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "guardian.backends.ObjectPermissionBackend",
+)
