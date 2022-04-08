4b031552538ea1458a6583f76e00db3d1305afdd
Louis Agyapong <agyapong.louis@gmail.com>
2022-04-08 00:25:10 +0000

    django guardian object level permission

--- apps/inventory/admin.py
+++ apps/inventory/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.http import HttpRequest
 from django.utils import timezone
 from guardian.admin import GuardedModelAdmin
+from guardian.shortcuts import get_objects_for_user
 
 from .models import Product
 
@@ -62,11 +63,24 @@ class ProductAdmin(GuardedModelAdmin):
     prepopulated_fields = {"slug": ("web_id",)}
 
     def get_queryset(self, request):
-        return super().get_queryset(request)
+        if request.user.is_superuser:
+            return super().get_queryset(request)
+        data = self.get_model_objects(request)
+        return data
 
     def has_module_permission(self, request: HttpRequest) -> bool:
         if super().has_module_permission(request):
             return True
+        return self.get_model_objects(request).exists()
+
+    def get_model_objects(self, request, action=None, klass=None):
+        opts = self.opts
+        actions = [action] if action else ["view", "edit", "delete"]
+        klass = klass if klass else opts.model
+        model_name = klass._meta.model_name
+        return get_objects_for_user(
+            user=request.user, perms=[f"{perm}_{model_name}" for perm in actions], klass=klass, any_perm=True
+        )
 
     def has_permission(self, request: HttpRequest, obj, action) -> bool:
         opts = self.opts
@@ -74,12 +88,14 @@ class ProductAdmin(GuardedModelAdmin):
 
         if obj:
             return request.user.has_perm(f"{opts.app_label}.{code_name}", obj)
+        else:
+            return self.get_model_objects(request).exists()
 
     def has_view_permission(self, request: HttpRequest, obj=None) -> bool:
         return self.has_permission(request, obj, "view")
 
-    def has_add_permission(self, request: HttpRequest) -> bool:
-        return self.has_permission(request, None, "add")
+    # def has_add_permission(self, request: HttpRequest) -> bool:
+    #     return self.has_permission(request, None, "add")
 
     def has_change_permission(self, request: HttpRequest, obj=None) -> bool:
         return self.has_permission(request, obj, "change")
